9460ff2380c127351619eb45ec453a6d139e3f2c
Jordan C <nykeztv@users.noreply.github.com>
2020-07-08 02:58:24 -0400

    add alert that this is a <demo> website

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -2,12 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
 import { Router } from '@angular/router';
+import { AlertModule } from 'ngx-bootstrap/alert';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
+
 export class NavComponent implements OnInit {
   model: any = {};
 
