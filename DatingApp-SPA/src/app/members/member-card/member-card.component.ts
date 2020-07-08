a08ab364ccf79128fb7c607197b288f85e21f0c8
Jordan C <nykeztv@users.noreply.github.com>
2020-07-07 21:19:00 -0400

    memberlist, member compoents, styling

---
+++ DatingApp-SPA/src/app/members/member-card/member-card.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { User } from '../../_models/user';
+import { AuthService } from '../../_services/auth.service';
+import { UserService } from '../../_services/user.service';
+import { AlertifyService } from '../../_services/alertify.service';
+
+@Component({
+  selector: 'app-member-card',
+  templateUrl: './member-card.component.html',
+  styleUrls: ['./member-card.component.css']
+})
+export class MemberCardComponent implements OnInit {
+  @Input() user: User;
+
+  constructor(private authService: AuthService, private userService: UserService, private alertify: AlertifyService) { }
+
+  ngOnInit() {
+  }
+
+}
