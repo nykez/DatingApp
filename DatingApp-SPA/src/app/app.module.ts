14754ec9fb797ca0f3504bd2a42316c55976a9c2
Jordan C <nykeztv@users.noreply.github.com>
2020-07-07 21:50:08 -0400

    user detail page

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -19,6 +19,7 @@ import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { appRoutes } from './routes';
 import { MemberCardComponent } from './members/member-card/member-card.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 
 export function tokenGetter() {
    return localStorage.getItem('token');
@@ -34,6 +35,7 @@ export function tokenGetter() {
       ListsComponent,
       MessagesComponent,
       MemberCardComponent,
+      MemberDetailComponent,
    ],
    imports: [
       BrowserModule,
