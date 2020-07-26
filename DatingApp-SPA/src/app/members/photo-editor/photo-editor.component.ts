a4409811a1078adc16788b8e334677c26ebb9049
Jordan C <nykeztv@users.noreply.github.com>
2020-07-25 20:41:58 -0400

    add delete spa methods. finished photo editor

--- DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
+++ DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
@@ -77,5 +77,14 @@ export class PhotoEditorComponent implements OnInit {
       this.alertify.error(error);
     });
   }
-  
+
+  deletePhoto(id: number){
+    this.alertify.confirm('Are you sure you want to delete this photo?', () => {
+      this.userService.deletePhoto(this.authService.decodedToken.nameid, id).subscribe(() => {
+        this.photos.splice(this.photos.findIndex(p => p.id === id), 1);
+      }, error => {
+        this.alertify.error(error);
+      });
+    });
+  }
 }
